dba7f16458204bab23d76eb8e61e45896c0a5826
danielpolk <danielpolk@mac.com>
2019-02-25 20:56:36 -0600

    updated design and content and root files

---
+++ assets/js/script.js
@@ -0,0 +1,200 @@
+// var configDB = {
+//     apiKey: config.apiKey,
+//     authDomain: config.authDomain,
+//     databaseURL: config.databaseURL,
+//     projectId: config.projectId,
+//     storageBucket: "",
+//     messagingSenderId: config.messagingSenderId
+//   };
+
+
+
+// firebase.initializeApp(configDB);
+
+
+
+// var messagesRef = firebase.database().ref('messages')
+
+// document.getElementById('contactForm').addEventListener('submit', submitForm);
+// function submitForm(e){
+//   e.preventDefault();
+
+//   var name = getInputVal('name');
+//   var email = getInputVal('email');
+//   var phone = getInputVal('phone');
+//   var message = getInputVal('message');
+
+
+//   $('#name').val('');
+//   $('#email').val('');
+//   $('#phone').val('');
+//   $('#message').val('');
+
+//   saveMessage(name,email,phone,message);
+
+//   document.querySelector('.alert').style.display = 'block';
+//   setTimeout(function(){
+//     document.querySelector('.alert').style.display = 'none';
+//   },3000)
+// }
+
+// function getInputVal(id){
+//   return document.getElementById(id).value;
+// }
+
+// function saveMessage(name,email,phone,message){
+//   var newMessageRef = messagesRef.push();
+//   newMessageRef.set({
+//     name:name,
+//     email:email,
+//     phone:phone,
+//     message:message
+//   })
+// }
+
+// text effect
+var TxtType = function(el, toRotate, period) {
+    console.log("this works!!");
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = "";
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = "<span class=\"wrap\">"+this.txt+"</span>";
+
+    var that = this;
+    var delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) { delta /= 2; }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === "") {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function() {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function() {
+    var elements = document.getElementsByClassName("typewrite");
+    for (var i=0; i<elements.length; i++) {
+        var toRotate = elements[i].getAttribute("data-type");
+        var period = elements[i].getAttribute("data-period");
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $("a.js-scroll-trigger[href*=\"#\"]:not([href=\"#\"])").click(function() {
+        if (location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+            if (target.length) {
+                $("html, body").animate({
+                    scrollTop: (target.offset().top - 54)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $(".js-scroll-trigger").click(function() {
+        $(".navbar-collapse").collapse("hide");
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $("body").scrollspy({
+        target: "#mainNavbar",
+        offset: 56
+    });
+
+    // Collapse Navbar
+    var navbarCollapse = function() {
+        if ($("#mainNavbar").offset().top > 700) {
+            $("#mainNavbar").addClass("navbar-shrink navbar-item-transparent");
+        } else {
+            $("#mainNavbar").removeClass("navbar-shrink navbar-item-transparent");
+        }
+    };
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
+
+    // Hide navbar when modals trigger
+    $(".portfolio-modal").on("show.bs.modal", function(e) {
+        $(".navbar").addClass("d-none");
+    });
+    $(".portfolio-modal").on("hidden.bs.modal", function(e) {
+        $(".navbar").removeClass("d-none");
+    });
+
+})(jQuery); // End of use strict
+
+
+
+$("#navAbout").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#about").offset().top
+    }, 2000);
+});
+$("#navResume").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#resume").offset().top
+    }, 2000);
+});
+$("#navSkills").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#skills").offset().top
+    }, 2000);
+});
+$("#navPortfolio").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#portfolio").offset().top
+    }, 2000);
+});
+$("#navContact").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#contact").offset().top
+    }, 2000);
+});
+$("#learnMore").click(function() {
+    $("html, body").animate({
+        scrollTop: $("#about").offset().top
+    }, 1000);
+});
+
+
+
